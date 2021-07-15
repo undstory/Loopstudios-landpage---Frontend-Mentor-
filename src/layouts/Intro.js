a76a93254de9ddf606c51664bf6a351af1520c28
undstory <undrul.ozog@gmail.com>
2021-07-15 17:44:27 +0200

    intro media works

--- src/layouts/Intro.js
+++ src/layouts/Intro.js
@@ -12,32 +12,84 @@ const StyledWrapper = styled.section`
     flex-direction: row;
     align-items: center;
     justify-content: center;
+
+    @media (max-width: 997px) {
+    padding-top: 10rem;
+    }
 `;
 
 const ContentWrapper = styled.div`
     width: 1440px;
     margin: 0 auto;  
     position: relative;
+
+    @media (max-width: 997px) {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    justify-content: center;
+    }
 `;
 
 const Img = styled.img`
     width: 70%;
+
+    @media (max-width: 997px) {
+   
+        width: 90%;
+    }
 `;
 
 const StyledBox = styled.div`
-    padding: 10rem 0 0 10rem;
+    padding: 10rem 10rem 0 10rem;
     background-color: ${({ theme }) => theme.white};
     position: absolute;
     bottom: 0; 
     right: 0;
     z-index: 3000;
     width: 50%;
+
+    @media (max-width: 1220px) {
+        padding: 5rem 5rem 0 5rem;
+    }
+
+    @media (max-width: 997px) {
+        position: static;
+        width: 80%;
+    }
+
+    @media (max-width: 427px) {
+        position: static;
+        width: 90%;
+    }
 `;
 
 const TitleMini = styled(Title)`
-    font-size: 2.8vw;
+    font-size: 5rem;
     color: ${({ theme }) => theme.black};
     margin-bottom: 3rem;
+
+    @media (max-width: 997px) {
+        text-align: center;
+        font-size: 3rem;
+    }
+
+    @media (max-width: 427px) {
+       font-size: 2rem;
+    }
+`;
+
+const StyledParagraph = styled(Paragraph)`
+    
+    @media (max-width: 997px) {
+        text-align: center;
+        font-size: 1.5rem;
+    }
+  
+    @media (max-width:425px) {
+     
+        font-size: 1.2rem;
+    }
 `;
 
 
@@ -47,10 +99,10 @@ const Intro = () => (
             <Img src={img} alt="man with interactive vr" />
             <StyledBox>
                 <TitleMini>The leader in interactive vr</TitleMini>
-                <Paragraph>Founded in 2011, Loopstudios has beem producing world-class virtual 
+                <StyledParagraph>Founded in 2011, Loopstudios has beem producing world-class virtual 
                     reality projects for some of the best companies around the globe. 
                     Our award-winning creations have transformed businesses through digital 
-                    experiences that bind to their brand.</Paragraph>
+                    experiences that bind to their brand.</StyledParagraph>
             </StyledBox>
         </ContentWrapper>
  
